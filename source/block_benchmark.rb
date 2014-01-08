8336b436166ddc10649b9f2e7d5e87c3c0e3ad78
Apprentice <apprentice@dbc02.local>
2014-01-07 18:31:36 -0800

    finished with yield

--- source/block_benchmark.rb
+++ source/block_benchmark.rb
@@ -1,9 +1,18 @@
 def benchmark
-  # Your benchmarking code goes here.
+  start =Time.now
+
+  yield
+
+ endtime = Time.now
+
+ endtime - start
 end
 
 # Be careful, pasting this into IRB will take a long time to print.
 # It's a loooong string. :)
+
+
+
 long_string = "apple"*100000000
 
 running_time = benchmark { long_string.reverse }
